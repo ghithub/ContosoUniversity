8e51fb7f8295dc47edec1969dcbda5dfe35350ec
Ghithub <gnewsgroup@gmail.com>
2016-03-15 15:55:43 -0400

    Added details controller action method and the corredsponding view.

--- ContosoUniversity/Controllers/StudentController.cs
+++ ContosoUniversity/Controllers/StudentController.cs
@@ -1,8 +1,11 @@
 ﻿using ContosoUniversity.DAL;
+using ContosoUniversity.Models;
 using ContosoUniversity.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -12,6 +15,46 @@ namespace ContosoUniversity.Controllers {
         public ActionResult Index() {
             StudentsViewModel vm = new StudentsViewModel();
             return View(vm);
-        }       
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            DetailsViewModel vm = new DetailsViewModel(id);
+
+            if (vm.Student == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(vm);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "LastName, FirstMidName, EnrollmentDate")]Student student)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    //Modify: Have the view model do the save.
+                    //db.Students.Add(student);
+                    //db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch (DataException /* dex */)
+            {
+                //Log the error (uncomment dex variable name and add a line here to write a log.
+                ModelState.AddModelError("", "Unable to save changes. Try again, and if the problem persists see your system administrator.");
+            }
+            return View(student);
+        }
+
     }
 }
---
+++ ContosoUniversity/ViewModels/DetailsViewModel.cs
@@ -0,0 +1,22 @@
+﻿using ContosoUniversity.DAL;
+using ContosoUniversity.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.ViewModels
+{
+    public class DetailsViewModel
+    {
+        private SchoolContext db = new SchoolContext();
+        public Student Student { get; set; }
+        public string Title = "Details";
+
+        public DetailsViewModel(int? id)
+        {
+            Student = db.Students.Find(id);
+        }
+    }
+}
