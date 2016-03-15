044686964c0f472a2f6ffd3c274f35320beb57e9
Ghithub <gnewsgroup@gmail.com>
2016-03-15 11:52:05 -0400

    Added student view model. Note I am not strictly following the code by Tom at asp.net.

---
+++ ContosoUniversity/Controllers/StudentController.cs
@@ -0,0 +1,17 @@
+﻿using ContosoUniversity.DAL;
+using ContosoUniversity.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ContosoUniversity.Controllers {
+    public class StudentController : Controller
+    {
+        public ActionResult Index() {
+            StudentsViewModel vm = new StudentsViewModel();
+            return View(vm);
+        }       
+    }
+}
---
+++ ContosoUniversity/ViewModels/StudentsViewModel.cs
@@ -0,0 +1,23 @@
+﻿using ContosoUniversity.DAL;
+using ContosoUniversity.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.ViewModels
+{
+    public class StudentsViewModel
+    {
+        private SchoolContext db = new SchoolContext();
+        public Student Student { get; set; }
+        public IList<Student> Students { get; set; }
+
+        public StudentsViewModel()
+        {
+            Student = new Student();
+            Students = db.Students.ToList();
+        }
+    }
+}
