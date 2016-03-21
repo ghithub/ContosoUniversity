8a125a97bfc3e8226a6d9508850d93773bbf3145
Ghithub <gnewsgroup@gmail.com>
2016-03-21 11:19:09 -0400

    Completed the sorting feature. I didn't use the approach in the tutorial because the tutorial throws everthing into the controller action method, which is not considered good practice.

--- ContosoUniversity/ViewModels/StudentsViewModel.cs
+++ ContosoUniversity/ViewModels/StudentsViewModel.cs
@@ -16,7 +16,8 @@ namespace ContosoUniversity.ViewModels
         public string NameSortOrder { get; set; }
         public string DateSortOrder { get; set; }
 
-        public StudentsViewModel(){
+        public StudentsViewModel()
+        {
         }
 
         public StudentsViewModel(string sortOrder)
@@ -26,15 +27,25 @@ namespace ContosoUniversity.ViewModels
 
             switch (sortOrder)
             {
+                case "":
+                case "name_asc":
+                    NameSortOrder = "name_desc";
+                    DateSortOrder = "date_asc";
+                    Students = Students.OrderBy(s => s.LastName).ToList();
+                    break;
                 case "name_desc":
-                    NameSortOrder = sortOrder;
+                    NameSortOrder = "name_asc";
+                    DateSortOrder = "date_asc";
                     Students = Students.OrderByDescending(s => s.LastName).ToList();
                     break;
-                case "Date":
-                    DateSortOrder = sortOrder;
+                case "date_asc":
+                    NameSortOrder = "name_asc";
+                    DateSortOrder = "date_desc";
                     Students = Students.OrderBy(s => s.EnrollmentDate).ToList();
                     break;
                 case "date_desc":
+                    NameSortOrder = "name_asc";
+                    DateSortOrder = "date_asc";
                     Students = Students.OrderByDescending(s => s.EnrollmentDate).ToList();
                     break;
                 default:
